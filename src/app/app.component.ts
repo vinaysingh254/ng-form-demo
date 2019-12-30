434471aeeb3ff433ce362e9cce75f02cb7190187
vinaysingh913 <vinaysingh.singh254@gmail.com>
2019-12-31 00:13:12 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-form-demo';
+  @ViewChild('f', {static: false}) signupForm: NgForm;
+  submitted = false;
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['Male', 'Female'];
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    questionAnswer: '',
+    gender: ''
+  };
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    /* this.signupForm.setValue({
+       userData: {
+         username: suggestedName,
+         email: ''
+       },
+       secret: this.defaultQuestion,
+       questionAnswer: '',
+       gender: 'Male'
+     });*/
+
+    // override part of value
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  onSubmit(f: NgForm) {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.questionAnswer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+  }
 }
